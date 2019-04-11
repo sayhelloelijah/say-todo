3ae5138c5b81530dfa5cf0810069c5ed9589e3b9
sayhelloelijah <say@helloelijah.com>
2019-04-11 16:12:02 -0500

    add completion toggle; delete functionality

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     const tasksList = document.querySelector('.todo__list');
     const taskButton = document.querySelector('.todo__add-task');
-    const taskForm = document.querySelector('.todo__dialog-form')
-    const closeButton = document.querySelector('.todo__dialog-cancel');
+    const taskForm = document.querySelector('.todo__dialog-form');
 
     function addTask(e) {
         e.preventDefault();
@@ -29,26 +28,23 @@ document.addEventListener('DOMContentLoaded', () => {
           return `
             <li class="todo__list-item ${task.done ? "todo__list-item--done" : ""}" data-index="${i}">
               ${task.text}
-              <svg class="todo__list-item-svg" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 59 59" style="enable-background:new 0 0 59 59;" xml:space="preserve"><g><path d="M29.5,51c0.552,0,1-0.447,1-1V17c0-0.553-0.448-1-1-1s-1,0.447-1,1v33C28.5,50.553,28.948,51,29.5,51z"/><path d="M19.5,51c0.552,0,1-0.447,1-1V17c0-0.553-0.448-1-1-1s-1,0.447-1,1v33C18.5,50.553,18.948,51,19.5,51z"/><path d="M39.5,51c0.552,0,1-0.447,1-1V17c0-0.553-0.448-1-1-1s-1,0.447-1,1v33C38.5,50.553,38.948,51,39.5,51z"/><path d="M52.5,6H38.456c-0.11-1.25-0.495-3.358-1.813-4.711C35.809,0.434,34.751,0,33.499,0H23.5c-1.252,0-2.31,0.434-3.144,1.289 C19.038,2.642,18.653,4.75,18.543,6H6.5c-0.552,0-1,0.447-1,1s0.448,1,1,1h2.041l1.915,46.021C10.493,55.743,11.565,59,15.364,59 h28.272c3.799,0,4.871-3.257,4.907-4.958L50.459,8H52.5c0.552,0,1-0.447,1-1S53.052,6,52.5,6z M21.792,2.681 C22.24,2.223,22.799,2,23.5,2h9.999c0.701,0,1.26,0.223,1.708,0.681c0.805,0.823,1.128,2.271,1.24,3.319H20.553 C20.665,4.952,20.988,3.504,21.792,2.681z M46.544,53.979C46.538,54.288,46.4,57,43.636,57H15.364 c-2.734,0-2.898-2.717-2.909-3.042L10.542,8h37.915L46.544,53.979z"/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
+              <svg class="todo__list-item-svg" data-index="${i}" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 59 59" style="enable-background:new 0 0 59 59;" xml:space="preserve"><g><path d="M29.5,51c0.552,0,1-0.447,1-1V17c0-0.553-0.448-1-1-1s-1,0.447-1,1v33C28.5,50.553,28.948,51,29.5,51z"/><path d="M19.5,51c0.552,0,1-0.447,1-1V17c0-0.553-0.448-1-1-1s-1,0.447-1,1v33C18.5,50.553,18.948,51,19.5,51z"/><path d="M39.5,51c0.552,0,1-0.447,1-1V17c0-0.553-0.448-1-1-1s-1,0.447-1,1v33C38.5,50.553,38.948,51,39.5,51z"/><path d="M52.5,6H38.456c-0.11-1.25-0.495-3.358-1.813-4.711C35.809,0.434,34.751,0,33.499,0H23.5c-1.252,0-2.31,0.434-3.144,1.289 C19.038,2.642,18.653,4.75,18.543,6H6.5c-0.552,0-1,0.447-1,1s0.448,1,1,1h2.041l1.915,46.021C10.493,55.743,11.565,59,15.364,59 h28.272c3.799,0,4.871-3.257,4.907-4.958L50.459,8H52.5c0.552,0,1-0.447,1-1S53.052,6,52.5,6z M21.792,2.681 C22.24,2.223,22.799,2,23.5,2h9.999c0.701,0,1.26,0.223,1.708,0.681c0.805,0.823,1.128,2.271,1.24,3.319H20.553 C20.665,4.952,20.988,3.504,21.792,2.681z M46.544,53.979C46.538,54.288,46.4,57,43.636,57H15.364 c-2.734,0-2.898-2.717-2.909-3.042L10.542,8h37.915L46.544,53.979z"/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
             </li>
           `
         }).join('');
-      }
+        isEmpty();
+    }
 
     function isEmpty() {
-        console.group('isEmpty');
-            if(tasksList.innerHTML === "") {
-                console.log('Empty');
-                tasksList.classList.add('todo__list--empty');
-            } else {
-                console.log('NOT Empty');
-                tasksList.classList.remove('todo__list--empty');
-            }
-        console.groupEnd();
+        if(tasksList.innerHTML === "") {
+            tasksList.classList.add('todo__list--empty');
+        } else {
+            tasksList.classList.remove('todo__list--empty');
+        }
     }
 
     function toggleDone(e) {
-        if(!e.target.matches('input')) return;
+        if(!e.target.matches('.todo__list-item')) return;
         const el = e.target;
         const index = el.dataset.index;
         tasks[index].done = !tasks[index].done;
@@ -56,6 +52,16 @@ document.addEventListener('DOMContentLoaded', () => {
         populateList(tasks, tasksList);
     }
 
+    function deleteItem(e) {
+        if(!e.target.matches('.todo__list-item-svg')) return;
+        const el = e.target;
+        const index = el.dataset.index;
+        tasks.splice(index, 1);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        populateList(tasks, tasksList);
+    }
+
+    populateList(tasks, tasksList);
     taskButton.addEventListener('click', () => {
         dialog.showModal();
     });
@@ -66,6 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
             e.preventDefault();
         }
     });
-    populateList(tasks, tasksList);
-    isEmpty();
+
+    tasksList.addEventListener('click', toggleDone);
+    tasksList.addEventListener('click', deleteItem);
 });
